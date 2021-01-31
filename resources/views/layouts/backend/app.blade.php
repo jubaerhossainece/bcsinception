dd344002db2470317d2b927efcb0e67a47c5c2a7
jubaerhossainece <jubaerkuet@gmail.com>
2021-02-01 01:08:42 +0600

    vendor-all-min.js file pulled up of bootstrap.js

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -55,8 +55,8 @@
 	<!-- Required Js -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" ></script>
 	<script src="{{url('assets/js/vendor-all.min.js')}}"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" ></script>
 	
 	<script src="{{url('assets/js/pcoded.min.js')}}"></script>
 	<script src="{{url('js/script.js')}}"></script>
